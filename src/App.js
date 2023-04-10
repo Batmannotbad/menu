7daf69845d6b7558ee41bd14d7e452e5b07b81ba
Batmannotbad <lebaovannn@gmail.com>
2023-04-10 14:02:05 +0700

    checkout-form

--- src/App.js
+++ src/App.js
@@ -12,11 +12,15 @@ class App extends React.Component {
         super ();
         this.state = {
             products: data.products,
-            cartItems:[],
+            cartItems:localStorage.getItem("cartItems")? JSON.parse(localStorage.getItem("cartItems"))
+            :[],
             type:"",
             sort:"",
         };
     }
+    createOrder = (order) => {
+        alert("Need to save order for " + order.name);
+      };
     increItem = (product)=>{
         const cartItems = this.state.cartItems.slice();
         cartItems.forEach(item =>{
@@ -25,6 +29,9 @@ class App extends React.Component {
             }
         });
         this.setState({cartItems})
+        localStorage.setItem("cartItems",JSON.stringify(cartItems));
+
+
     };
     decreItem = (product)=>{
         const cartItems = this.state.cartItems.slice();
@@ -34,12 +41,18 @@ class App extends React.Component {
             }
         });
         this.setState({cartItems})
+        localStorage.setItem("cartItems",JSON.stringify(cartItems));
+
     };
     removeFromCart = (product) => {
         const cartItems = this.state.cartItems.slice();
         this.setState({
           cartItems: cartItems.filter((x) => x._id !== product._id),
         });
+        localStorage.setItem(
+            "cartItems",
+            JSON.stringify(cartItems.filter((x) => x._id !== product._id))
+          );
       };
     addToCart = (product)=>{
         const cartItems = this.state.cartItems.slice();
@@ -54,6 +67,7 @@ class App extends React.Component {
             cartItems.push({...product, count: 1});
         }
         this.setState({cartItems})
+        localStorage.setItem("cartItems",JSON.stringify(cartItems));
     };
     sortProducts = (event) => {
         // impl
@@ -163,6 +177,7 @@ class App extends React.Component {
                 increItem={this.increItem}
                 decreItem={this.decreItem}
                 removeFromCart={this.removeFromCart}
+                createOrder={this.createOrder}
               />
             </div>
           </div>
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,84 +1,194 @@
 import React, { Component } from "react";
 // import formatCurrency from "../util";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
 
 export default class Cart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      phone: "",
+      address: "",
+      note:"",
+      name:"",
+      showCheckout: false,
+    };
+  }
+  handleFormSubmit = (values) => {
+    const order = {
+      name: values.name,
+      phone: values.phone,
+      address: values.address,
+      note: values.note,
+      cartItems: this.props.cartItems,
+    };
+    this.props.createOrder(order);
+  };
+  // handleInput = (e) => {
+  //   this.setState({ [e.target.name]: e.target.value });
+  // };
+  
+  createOrder = (values, { resetForm }) => {
+    const order = {
+      name: values.name,
+      phone: values.phone,
+      address: values.address,
+      note: values.note,
+      cartItems: this.props.cartItems,
+    };
+    this.props.createOrder(order);
+    resetForm();
+    this.setState({ showCheckout: false });
+  };
   render() {
     const { cartItems } = this.props;
-    console.log('cartItems:', this.props.cartItems);
-    console.log('cart:', this.props.cart);
-    console.log('updateCart:', this.props.updateCart);
-    console.log('increItem:', this.props.increItem);
-    console.log('decrementItem:', this.props.decrementItem);
-    console.log('removeFromCart:', this.props.removeFromCart);
-    return (
+    const initialValues = {
+      name: "",
+      phone: "",
+      address: "",
+      note: "",
+    };
+    
+    const validationSchema = Yup.object().shape({
+      name: Yup.string().required("Required"),
+      phone: Yup.string()
+        .required("Required")
+        .matches(/^[0-9]+$/, "Must be only digits")
+        .min(10, "Must be exactly 10 digits")
+        .max(10, "Must be exactly 10 digits"),
+      address: Yup.string().required("Required"),
+      note: Yup.string(),
+    });
+  return (
+
+    <div>
+      {cartItems.length === 0 ? (
+        <div className="cart cart-header">Cart is empty</div>
+      ) : (
+        <div className="cart cart-header">
+          You have {cartItems.length} in the cart{" "}
+        </div>
+      )}
       <div>
-        {cartItems.length === 0 ? (
-          <div className="cart cart-header">Cart is empty</div>
-        ) : (
-          <div className="cart cart-header">
-            You have {cartItems.length} in the cart{" "}
-          </div>
-        )}
-        <div>
-          <div className="cart">
-            <ul className="cart-items">
-              {cartItems.map((item) => (
-                <li key={item._id}>
-                  <div className="productInfo">
-                    <div>
-                      <img src={item.picturePath} alt={item.name}></img>
-                    </div>
-                    <div className="info">
-                      <div>{item.name}</div>
-                      <div>
-                            {item.price + " đ"}
-                      </div>
-                    </div>
-                  </div>
+        <div className="cart">
+          <ul className="cart-items">
+            {cartItems.map((item) => (
+              <li key={item._id}>
+                <div className="productInfo">
                   <div>
-                    {/* <div>{item.name}</div> */}
-                    <div className="right">
-                      {/* <div>
-                        {item.price + " đ"}
-                      </div> */}
-                      <div className="itemCount">
-                        <button className="btn"
-                        onClick={()=> this.props.decreItem(item)}>
-                          -
-                        </button>
-                        <span> {item.count} </span>
-                        <button className="btn"
-                        onClick={()=> this.props.increItem(item)}>
-                          +
-                        </button>
-                      </div>
+                    <img src={item.picturePath} alt={item.name}></img>
+                  </div>
+                  <div className="info">
+                    <div>{item.name}</div>
+                    <div>{item.price + " đ"}</div>
+                  </div>
+                </div>
+                <div>
+                  <div className="right">
+                    <div className="itemCount">
                       <button
                         className="btn"
-                        onClick={() => this.props.removeFromCart(item)}>
-                        Remove
+                        onClick={() => this.props.decreItem(item)}
+                      >
+                        -
+                      </button>
+                      <span> {item.count} </span>
+                      <button
+                        className="btn"
+                        onClick={() => this.props.increItem(item)}
+                      >
+                        +
                       </button>
                     </div>
+                    <button
+                      className="btn"
+                      onClick={() => this.props.removeFromCart(item)}
+                    >
+                      Remove
+                    </button>
                   </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-          {cartItems.length !== 0 && (
-            <div className="cart">
-              <div className="total">
-                <div>
-                  Total: {" "}
-                  {
-                    cartItems.reduce((a, c) => a + c.price * c.count, 0)
-                  }
-                  {" đ"}
                 </div>
-                </div>
-                <button className="btn primary">Proceed</button>
-              </div>
-          )}
+              </li>
+            ))}
+          </ul>
         </div>
-      </div>
-    );
+        {cartItems.length !== 0 && (
+          <div className="cart">
+            <div className="total">
+              <div>
+                Total:{" "}
+                {cartItems.reduce((a, c) => a + c.price * c.count, 0)} {" đ"}
+              </div>
+            </div>
+            <button
+              onClick={() => {
+                this.setState({ showCheckout: true });
+              }}
+              className="btn primary"
+            >
+              Proceed
+            </button>
+          </div>
+        )}
+        </div>
+        {this.state.showCheckout && (
+    <div className="cart">
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={this.createOrder}
+      >
+        {({ errors, touched }) => (
+          <Form className="checkoutForm">
+            <div>
+              <label>Name</label>
+              <Field
+                name="name"
+                placeholder="Enter your name"
+                className={errors.name && touched.name ? 'error' : null}
+              />
+              <ErrorMessage name="name" component="div" className="error-message" />
+            </div>
+
+            <div>
+              <label>Phone</label>
+              <Field
+                name="phone"
+                placeholder="Enter your phone number"
+                className={errors.phone && touched.phone ? 'error' : null}
+              />
+              <ErrorMessage name="phone" component="div" className="error-message" />
+            </div>
+
+            <div>
+              <label>Address</label>
+              <Field
+                name="address"
+                placeholder="Enter your address"
+                className={errors.address && touched.address ? 'error' : null}
+              />
+              <ErrorMessage name="address" component="div" className="error-message" />
+            </div>
+
+            <div>
+              <label>Note</label>
+              <Field
+                name="note"
+                placeholder="Add a note for your order (optional)"
+                as="textarea"
+              />
+            </div>
+
+            <button type="submit" className="btn primary">
+              Place Order
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  )}
+    </div>
+  );
   }
-}
+  }
